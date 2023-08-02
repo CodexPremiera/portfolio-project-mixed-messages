1b6b1757edaa6ae5283fd1901601720cf33c12cc
Ashley Ken Comandao <ashleyken.professional@gmail.com>
2023-08-02 17:27:02 +0800

    Create template for the statement bank

--- mixed_messages.js
+++ mixed_messages.js
@@ -2,3 +2,95 @@
 const generateRandomNumber = range =>
     Math.floor(Math.random() * range);
 
+/* ==================== STATEMENT BANK ==================== */
+const statements = {
+    _ancient: {
+        person1: {
+            name: '',
+            quote: '',
+        },
+        person2: {
+            name: '',
+            quote: '',
+        },
+        person3: {
+            name: '',
+            quote: '',
+        },
+        person4: {
+            name: '',
+            quote: '',
+        },
+        person5: {
+            name: '',
+            quote: '',
+        },
+    },
+    _eastern: {
+        person1: {
+            name: '',
+            quote: '',
+        },
+        person2: {
+            name: '',
+            quote: '',
+        },
+        person3: {
+            name: '',
+            quote: '',
+        },
+        person4: {
+            name: '',
+            quote: '',
+        },
+        person5: {
+            name: '',
+            quote: '',
+        },
+    },
+    _renaissance: {
+        person1: {
+            name: '',
+            quote: '',
+        },
+        person2: {
+            name: '',
+            quote: '',
+        },
+        person3: {
+            name: '',
+            quote: '',
+        },
+        person4: {
+            name: '',
+            quote: '',
+        },
+        person5: {
+            name: '',
+            quote: '',
+        },
+    },
+    _contemporary: {
+        person1: {
+            name: '',
+            quote: '',
+        },
+        person2: {
+            name: '',
+            quote: '',
+        },
+        person3: {
+            name: '',
+            quote: '',
+        },
+        person4: {
+            name: '',
+            quote: '',
+        },
+        person5: {
+            name: '',
+            quote: '',
+        },
+    }
+}
+
